6319a6525c7ad72ccd288a41c914a18a9193f4fb
maiullyDev <maiully.dev@gmail.com>
2024-01-13 01:52:55 -0300

    Create product array

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 1,
+    name: "Vestido Vermelho",
+    category: "vestidos",
+    price: 150.0,
+    image: "/assets/productImages/dress-2.jpeg",
+  },
+  {
+    id: 2,
+    name: "Vestido Midi",
+    category: "vestidos",
+    price: 100.0,
+    image: "/assets/productImages/dress-1.jpeg",
+  },
+  {
+    id: 3,
+    name: "Scarpin",
+    category: "calcados",
+    price: 199.99,
+    image: "/assets/productImages/scarpin.jpeg",
+  },
+  {
+    id: 4,
+    name: "Tênis Cano Alto",
+    category: "calcados",
+    price: 269.99,
+    image: "/assets/productImages/shoes.png",
+  },
+  {
+    id: 5,
+    name: "Camiseta básica",
+    category: "camisas",
+    price: 24.99,
+    image: "/assets/productImages/shirt.png",
+  },
+  {
+    id: 6,
+    name: "Moletom",
+    category: "camisas",
+    price: 99.99,
+    image: "/assets/productImages/sweatshirt.png",
+  },
+];
